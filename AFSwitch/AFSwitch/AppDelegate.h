6f967176fd3ef6b309eb240c84404353f527e5d0
冯玉亭 <1358551681@qq.com>
2016-01-17 23:05:20 +0800

    添加switch工程

---
+++ AFSwitch/AFSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFSwitch
+//
+//  Created by 冯玉亭 on 16/1/17.
+//  Copyright © 2016年 冯玉亭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFSwitch/AFSwitch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFSwitch
+//
+//  Created by 冯玉亭 on 16/1/17.
+//  Copyright © 2016年 冯玉亭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
