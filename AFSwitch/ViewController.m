e954ab1731fa8002fef17a14816f3da6638ac0b7
fengyuting <fengyuting@mail.51vv.com>
2019-07-05 13:49:37 +0800

    feature：添加label

--- AFSwitch/ViewController.m
+++ AFSwitch/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc] init];
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
